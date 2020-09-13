d4922d36faa8877dae356505ff065e77eff57d79
Reuben Thomas <rrt@sc3d.org>
2020-09-13 16:22:13 +0100

    hakjs_example1.lua: use hakvm.Cell directly to shorten code

--- hakjs_example1.lua
+++ hakjs_example1.lua
@@ -5,31 +5,27 @@ local hakvm = require "hakvm"
 local code, cell
 
 -- An atom
-code = {atom = "hello"}
-cell = hakvm.Cell (code)
+cell = hakvm.Cell ({atom = "hello"})
 print (cell)
 --print (hakvm.evaluate (cell))
 
 -- An atom with empty children
-code = {atom = 1, children = {}}
-cell = hakvm.Cell (code)
+cell = hakvm.Cell ({atom = 1, children = {}})
 print (cell)
 --print (hakvm.evaluate (cell))
 
 -- An atom with some children
-code = {
+cell = hakvm.Cell ({
   atom = "hello",
   children = {{atom = 1, children = {"x", "y"}}, 2, 3},
-}
-cell = hakvm.Cell (code)
+})
 print (cell)
 --print (hakvm.evaluate (cell))
 
 -- Children with no atom
-code = {
+cell = hakvm.Cell ({
   children = {{atom = "hello"}}
-}
-cell = hakvm.Cell (code, {})
+})
 print (cell)
 --print (hakvm.evaluate (cell))
 
